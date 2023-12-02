a1ead4cacd656eba11fb820e65eddbc5608c3695
nhatvu148 <nhatvu148@gmail.com>
2023-12-02 15:26:17 +0700

    add test 2

---
+++ src/days/day_2.rs
@@ -0,0 +1,91 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+#[cfg(test)]
+#[path = "../unit_tests/day_2.rs"]
+mod day_2_tests;
+
+#[derive(Debug)]
+pub struct Game {
+    pub id: usize,
+    pub subsets: Vec<Subset>,
+}
+
+#[derive(Debug)]
+pub struct Subset {
+    pub counts: HashMap<String, usize>,
+}
+
+pub type CubeCounts = std::collections::HashMap<String, usize>;
+
+pub fn read_games_from_file(file_path: &str) -> Result<Vec<Game>, io::Error> {
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut games = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        if let Some((id, subsets)) = parse_game_line(&line) {
+            games.push(Game { id, subsets });
+        }
+    }
+
+    Ok(games)
+}
+
+fn parse_game_line(line: &str) -> Option<(usize, Vec<Subset>)> {
+    let parts: Vec<&str> = line.split(':').collect();
+    if parts.len() == 2 {
+        if let Ok(id) = parts[0]
+            .trim()
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse::<usize>()
+        {
+            let subsets = parts[1]
+                .split(';')
+                .flat_map(|subset_str| parse_subset(subset_str))
+                .collect();
+            return Some((id, subsets));
+        }
+    }
+    None
+}
+
+fn parse_subset(subset_str: &str) -> Option<Subset> {
+    let mut counts = HashMap::new();
+    let parts: Vec<&str> = subset_str.split(',').map(|s| s.trim()).collect();
+
+    for part in parts.iter() {
+        let p: Vec<&str> = part.split_whitespace().collect();
+        if let Ok(count) = p[0].parse::<usize>() {
+            let color = p[1..].join(" ").to_lowercase();
+            counts.insert(color, count);
+        }
+    }
+    Some(Subset { counts })
+}
+
+impl Game {
+    pub fn is_possible(&self, counts: &HashMap<String, usize>) -> bool {
+        for subset in &self.subsets {
+            for (color, count) in &subset.counts {
+                if let Some(&available_count) = counts.get(color) {
+                    if *count > available_count {
+                        return false;
+                    }
+                } else {
+                    return false; // Color not present in counts
+                }
+            }
+        }
+        true
+    }
+}
+
+pub fn is_game_possible(game: &Game, counts: &HashMap<String, usize>) -> bool {
+    game.is_possible(counts)
+}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,9 @@
-use advent_of_code_2023::days::day_1::*;
+use advent_of_code_2023::days::{
+    day_1::*,
+    day_2::{is_game_possible, read_games_from_file, CubeCounts},
+};
 
+#[ignore]
 #[test]
 fn test_day_1() {
     // Define a map of file paths and their expected totals
@@ -16,6 +20,43 @@ fn test_day_1() {
                 assert_eq!(total, *expected_total);
             }
             Err(err) => eprintln!("Error reading file {}: {}", file_path, err),
+        }
+    }
+}
+
+#[test]
+fn test_day_2() {
+    // Define a map of file paths and their expected sums
+    let test_cases = [("input/day_2_1.txt", 8), ("input/day_2_2.txt", 2679)];
+    let cube_counts: CubeCounts = [
+        ("red".to_string(), 12),
+        ("green".to_string(), 13),
+        ("blue".to_string(), 14),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    // Iterate through the test cases
+    for (file_path, expected_sum) in test_cases.iter() {
+        match read_games_from_file(file_path) {
+            Ok(games) => {
+                // for (i, game) in games.iter().enumerate() {
+                //     println!("Game {}: {:?}", i + 1, game);
+                // }
+
+                let possible_games: Vec<usize> = games
+                    .iter()
+                    .filter(|&game| is_game_possible(game, &cube_counts))
+                    .map(|game| game.id)
+                    .collect();
+
+                let sum: usize = possible_games.iter().sum();
+                println!("Possible games: {:?}", possible_games);
+                println!("Sum of IDs: {}", sum);
+                assert_eq!(sum, *expected_sum);
+            }
+            Err(err) => eprintln!("Error reading file: {}", err),
         }
     }
 }
