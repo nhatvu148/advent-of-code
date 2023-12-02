a79dc8ed821b09b841c82edabea0d86a53762678
nhatvu148 <nhatvu148@gmail.com>
2023-12-02 11:29:25 +0700

    edit test cases

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,27 +2,20 @@ use advent_of_code_2023::days::day_1::*;
 
 #[test]
 fn test_day_1() {
-    let file_path_1 = "input/day_1_1.txt";
-    match process_file(file_path_1) {
-        Ok(total) => {
-            assert_eq!(total, 142);
-        }
-        Err(err) => eprintln!("Error reading file: {}", err),
-    }
+    // Define a map of file paths and their expected totals
+    let test_cases = [
+        ("input/day_1_1.txt", 142),
+        ("input/day_1_2.txt", 54087),
+        ("input/day_1_3.txt", 281),
+    ];
 
-    let file_path_2 = "input/day_1_2.txt";
-    match process_file(file_path_2) {
-        Ok(total) => {
-            assert_eq!(total, 54087);
+    // Iterate through the test cases
+    for (file_path, expected_total) in test_cases.iter() {
+        match process_file(file_path) {
+            Ok(total) => {
+                assert_eq!(total, *expected_total);
+            }
+            Err(err) => eprintln!("Error reading file {}: {}", file_path, err),
         }
-        Err(err) => eprintln!("Error reading file: {}", err),
-    }
-
-    let file_path_3 = "input/day_1_3.txt";
-    match process_file(file_path_3) {
-        Ok(total) => {
-            assert_eq!(total, 281);
-        }
-        Err(err) => eprintln!("Error reading file: {}", err),
     }
 }
