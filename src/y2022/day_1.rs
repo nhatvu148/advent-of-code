d7dba53e58839798fff618694079cf09e8ee864e
nhatvu148 <nhatvu148@gmail.com>
2023-12-09 23:09:47 +0700

    finish day 1 2022

--- src/y2022/day_1.rs
+++ src/y2022/day_1.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-pub fn process_file(file_path: &str) -> u32 {
+pub fn process_file(file_path: &str) -> (u32, u32) {
     let mut batch: Vec<u32> = Vec::new();
     let mut sums: Vec<u32> = Vec::new();
 
@@ -34,6 +34,21 @@ pub fn process_file(file_path: &str) -> u32 {
         eprintln!("Error opening file: {}", file_path);
     }
 
-    sums.iter()
-        .fold(u32::MIN, |current_max, &x| current_max.max(x))
+    let max = sums
+        .iter()
+        .fold(u32::MIN, |current_max, &x| current_max.max(x));
+
+    let top3: Vec<u32> = sums.iter().fold(vec![], |mut acc, &x| {
+        if acc.len() < 3 {
+            acc.push(x);
+            acc.sort_by(|a, b| b.cmp(a)); // Sort in descending order
+        } else if x > *acc.last().unwrap() {
+            acc.pop();
+            acc.push(x);
+            acc.sort_by(|a, b| b.cmp(a)); // Sort in descending order
+        }
+        acc
+    });
+
+    (max, top3.iter().fold(0, |acc, &x| acc + x))
 }
--- tests/y2022.rs
+++ tests/y2022.rs
@@ -3,13 +3,14 @@ use advent_of_code::y2022::day_1::process_file;
 #[test]
 fn test_day_1() {
     let test_cases = [
-        ("input/y2022/day_1_1.txt", 24000),
-        ("input/y2022/day_1_2.txt", 69177),
+        ("input/y2022/day_1_1.txt", 24000, 45000),
+        ("input/y2022/day_1_2.txt", 69177, 207456),
     ];
 
-    for (file_path, expected_max) in test_cases.iter() {
-        let max = process_file(file_path);
+    for (file_path, expected_max, expected_sum_top_3) in test_cases.iter() {
+        let (max, sum_top_3) = process_file(file_path);
 
         assert_eq!(max, *expected_max);
+        assert_eq!(sum_top_3, *expected_sum_top_3);
     }
 }
