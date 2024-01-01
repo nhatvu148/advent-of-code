aceb77a18afb36bb555b6d0ca6b7dda9ae03af01
nhatvu148 <nhatvu148@gmail.com>
2024-01-01 12:39:18 +0700

    associated_types

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,78 @@
+#[derive(Debug)]
+struct Kmh {
+    value: u32,
+}
+
+#[derive(Debug)]
+struct Km {
+    value: u32,
+}
+
+#[derive(Debug)]
+struct Miles {
+    value: u32,
+}
+
+#[derive(Debug)]
+struct Mph {
+    value: u32,
+}
+
+/*
+impl Kmh {
+    fn distance_in_three_hours(&self) -> Km {
+        Km {
+            value: self.value * 3,
+        }
+    }
+}
+
+impl Mph {
+    fn distance_in_three_hours(&self) -> Miles {
+        Miles {
+            value: self.value * 3,
+        }
+    }
+}
+*/
+
+trait DistanceThreeHours {
+    type Distance;
+    fn distance_in_three_hours(&self) -> Self::Distance;
+}
+
+impl DistanceThreeHours for Kmh {
+    type Distance = Km;
+    fn distance_in_three_hours(&self) -> Self::Distance {
+        Self::Distance {
+            value: self.value * 3,
+        }
+    }
+}
+
+impl DistanceThreeHours for Mph {
+    type Distance = Miles;
+    fn distance_in_three_hours(&self) -> Self::Distance {
+        Self::Distance {
+            value: self.value * 3,
+        }
+    }
+}
+
+pub fn associated_types() {
+    let speed = Kmh { value: 90 };
+    let distance = speed.distance_in_three_hours();
+
+    println!("At {:?}, you will travel {:?} in 3 hours", speed, distance);
+
+    let speed_Mph = Mph { value: 90 };
+    let distance_Miles = speed_Mph.distance_in_three_hours();
+    println!(
+        "At {:?}, you will travel {:?} in 3 hours",
+        speed_Mph, distance_Miles
+    );
+}
+
 pub unsafe fn dangerous() {
     println!("a dangerous function call...");
 }
